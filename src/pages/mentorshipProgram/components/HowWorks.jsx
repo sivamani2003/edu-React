c8c11237f531c0e62ed389316608e60d0a71b8f7
EmranAli-15 <mohammademranali15@gmail.com>
2023-07-25 16:25:37 +0600

    some design changed again

--- src/pages/mentorshipProgram/components/HowWorks.jsx
+++ src/pages/mentorshipProgram/components/HowWorks.jsx
@@ -10,7 +10,7 @@ const HowWorks = () => {
                         </div>
 
                         <div className='md:flex mt-20'>
-                                <div className="card card-compact md:w-96 mx-5 md:mx-auto card-shadow myBorder">
+                                <div className="card card-compact md:w-96 mx-5 md:mx-auto bg-[#fdf9fd] card-shadow myBorder">
                                         <figure className='mt-2'>
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-color text-white p-2 w-12 h-12 rounded-md">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
@@ -23,7 +23,7 @@ const HowWorks = () => {
                                 </div>
 
 
-                                <div className="card card-compact md:w-96 mx-5 md:mx-auto card-shadow my-4 md:my-0 myBorder">
+                                <div className="card card-compact md:w-96 mx-5 md:mx-auto bg-[#fdf9fd] card-shadow my-4 md:my-0 myBorder">
                                         <figure className='mt-2'>
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-color text-white p-2 w-12 h-12 rounded-md">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
@@ -36,7 +36,7 @@ const HowWorks = () => {
                                 </div>
 
 
-                                <div className="card card-compact md:w-96 mx-5 md:mx-auto card-shadow myBorder">
+                                <div className="card card-compact md:w-96 mx-5 md:mx-auto bg-[#fdf9fd] card-shadow myBorder">
                                         <figure className='mt-2'>
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-color text-white p-2 w-12 h-12 rounded-md">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
--- src/pages/mentorshipProgram/components/WhyMentorship.jsx
+++ src/pages/mentorshipProgram/components/WhyMentorship.jsx
@@ -15,9 +15,9 @@ const WhyMentorship = () => {
                                                 <p className='text-[16px] md:text-2xl text-gray mt-4'>MENTORSHIP program promotes skill-sharing, knowledge transfer, and a sense of community among interns.</p>
                                         </div>
                                         <div className='mt-8'>
-                                                <div className='flex items-center gap-x-4'>
+                                                <div className='flex md:items-center gap-x-4'>
                                                         <div>
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-[#ee4962] shadow-lg text-white p-4 rounded-full w-16 h-20">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-[#ee4962] shadow-lg text-white p-4 rounded-full w-[50px] h-[65px]">
                                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
                                                                 </svg>
                                                         </div>
@@ -26,9 +26,9 @@ const WhyMentorship = () => {
                                                                 <p className='text lg md:text-xl text-gray'>Recognizes that interns possess unique talents and allowing interns to become teachers.</p>
                                                         </div>
                                                 </div>
-                                                <div className='flex items-center gap-x-4 my-6'>
+                                                <div className='flex md:items-center gap-x-4 my-6'>
                                                         <div>
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-[#ee4962] shadow-lg text-white p-4 rounded-full w-16 h-20">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-[#ee4962] shadow-lg text-white p-4 rounded-full w-[50px] h-[65px]">
                                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
                                                                 </svg>
                                                         </div>
@@ -37,9 +37,9 @@ const WhyMentorship = () => {
                                                                 <p className='text lg md:text-xl text-gray'>By teaching their peers, interns develop a deeper understanding of their subject matter. Explaining concepts, answering questions, and guiding others in their learning journey reinforces their own knowledge</p>
                                                         </div>
                                                 </div>
-                                                <div className='flex items-center gap-x-4'>
+                                                <div className='flex md:items-center gap-x-4'>
                                                         <div>
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-[#ee4962] shadow-lg text-white p-4 rounded-full w-16 h-20">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="bg-[#ee4962] shadow-lg text-white p-4 rounded-full w-[50px] h-[65px]">
                                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
                                                                 </svg>
                                                         </div>
