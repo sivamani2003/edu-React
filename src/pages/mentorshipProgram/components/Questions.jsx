e198520abb83afc427c9a8370a21635ef2e67c4e
EmranAli-15 <mohammademranali15@gmail.com>
2023-07-25 15:26:45 +0600

    collapse created

--- src/pages/mentorshipProgram/components/Questions.jsx
+++ src/pages/mentorshipProgram/components/Questions.jsx
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../Mentorship.css'
 import faq from '../../../assets/FAQs-amico.png'
 
 const Questions = () => {
 
-        const [toggle, setToggle] = useState(false)
-        const [toggleOne, setToggleOne] = useState(false)
-        const [toggleTwo, setToggleTwo] = useState(false)
+        const [toggle1, setToggle1] = useState(false);
+        const [toggle2, setToggle2] = useState(false);
+        const [toggle3, setToggle3] = useState(false);
+        const contentRef1 = useRef(null);
+        const contentRef2 = useRef(null);
+        const contentRef3 = useRef(null);
+
+        useEffect(() => {
+                if (contentRef1.current) {
+                        contentRef1.current.style.maxHeight = toggle1 ? `${contentRef1.current.scrollHeight}px` : '0';
+                }
+                if (contentRef2.current) {
+                        contentRef2.current.style.maxHeight = toggle2 ? `${contentRef2.current.scrollHeight}px` : '0';
+                }
+                if (contentRef3.current) {
+                        contentRef3.current.style.maxHeight = toggle3 ? `${contentRef3.current.scrollHeight}px` : '0';
+                }
+        }, [toggle2, toggle1, toggle3]);
+
+        const toggleHandle1 = () => {
+                setToggle1(!toggle1);
+        }
+        const toggleHandle2 = () => {
+                setToggle2(!toggle2);
+        }
+        const toggleHandle3 = () => {
+                setToggle3(!toggle3);
+        }
 
         const plus = <>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -19,7 +44,7 @@ const Questions = () => {
                 </svg></>
 
         return (
-                <div className='md:flex items-center'>
+                <div className='md:flex'>
                         <div className='md:w-1/2 hidden md:block'>
                                 <img className='w-[80%]' src={faq} alt="" />
                         </div>
@@ -28,45 +53,48 @@ const Questions = () => {
                                         <h1 className='text-xl md:text-5xl text-blue font-bold flex justify-center mb-6'>Frequently Asked <span className='text-pink ml-2'>Questions</span></h1>
                                 </div>
                                 <div>
-                                        <details className="collapse bg-base-200">
-                                                <summary onClick={() => setToggle(!toggle)} className="collapse-title text-xl font-medium">
-                                                        <div className='flex justify-between hover:text-[#ee4962] px-4'>
-                                                                <p>Q1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, doloremque??</p>
-                                                                {
-                                                                        toggle ? <>{minus}</> : <>{plus}</>
-                                                                }
-                                                        </div>
-                                                </summary>
-                                                <div className="collapse-content">
-                                                        <p className='px-4'>Yes, If you are aware of BAOIAM and believe in what we are doing, then feel free to participate.</p>
+                                        <div className='collapseContainer'>
+                                                <h1
+                                                        className='flex p-2 items-center justify-between cursor-pointer'
+                                                        onClick={toggleHandle1}>
+                                                        Q1. I am not a student of BAOIAM. Can I still participate?
+                                                        {toggle1 ? minus : plus}
+                                                </h1>
+                                                <div className={`animation ${toggle1 ? 'openCollapse' : ''} collapseContent`} ref={contentRef1}>
+                                                        <article className='article'>
+                                                                <div className="hr-line"></div>
+                                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita blanditiis dolorum consectetur dolores neque incidunt dolore perspiciatis officiis id quod.</p>
+                                                        </article>
                                                 </div>
-                                        </details>
-                                        <details className="collapse bg-base-200 my-4">
-                                                <summary onClick={() => setToggleOne(!toggleOne)} className="collapse-title text-xl font-medium">
-                                                        <div className='flex justify-between hover:text-[#ee4962] px-4'>
-                                                                <p>Q2. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error, cupiditate??</p>
-                                                                {
-                                                                        toggleOne ? <>{minus}</> : <>{plus}</>
-                                                                }
-                                                        </div>
-                                                </summary>
-                                                <div className="collapse-content">
-                                                        <p className='px-4'>You can see it on the website dashboard once you login.</p>
+                                        </div>
+                                        <div className='collapseContainer my-8'>
+                                                <h1
+                                                        className='flex p-2 items-center justify-between cursor-pointer'
+                                                        onClick={toggleHandle2}>
+                                                        Q2. How can I track the status of my earning?
+                                                        {toggle2 ? minus : plus}
+                                                </h1>
+                                                <div className={`animation ${toggle2 ? 'openCollapse' : ''} collapseContent`} ref={contentRef2}>
+                                                        <article>
+                                                                <div className="hr-line"></div>
+                                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita blanditiis dolorum consectetur dolores neque incidunt dolore perspiciatis officiis id quod.</p>
+                                                        </article>
                                                 </div>
-                                        </details>
-                                        <details className="collapse bg-base-200">
-                                                <summary onClick={() => setToggleTwo(!toggleTwo)} className="collapse-title text-xl font-medium">
-                                                        <div className='flex justify-between hover:text-[#ee4962] px-4'>
-                                                                <p>Q3. Lorem ipsum dolor sit amet consectetur.?</p>
-                                                                {
-                                                                        toggleTwo ? <>{minus}</> : <>{plus}</>
-                                                                }
-                                                        </div>
-                                                </summary>
-                                                <div className="collapse-content">
-                                                        <p className='px-4'>Anyone who has a skill expertise & has passion for teaching.Be it working professional or student.</p>
+                                        </div>
+                                        <div className='collapseContainer'>
+                                                <h1
+                                                        className='flex p-2 items-center justify-between cursor-pointer'
+                                                        onClick={toggleHandle3}>
+                                                        Q3. Who all can participate?
+                                                        {toggle3 ? minus : plus}
+                                                </h1>
+                                                <div className={`animation ${toggle3 ? 'openCollapse' : ''} collapseContent`} ref={contentRef3}>
+                                                        <article>
+                                                                <div className="hr-line"></div>
+                                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita blanditiis dolorum consectetur dolores neque incidunt dolore perspiciatis officiis id quod.</p>
+                                                        </article>
                                                 </div>
-                                        </details>
+                                        </div>
                                 </div>
                         </div>
                 </div>
