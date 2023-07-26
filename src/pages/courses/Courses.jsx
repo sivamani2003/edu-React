005d84055bfe10d072aabb9551b80b644580c76a
EmranAli-15 <mohammademranali15@gmail.com>
2023-07-26 18:47:16 +0600

    project section complete

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Home from './components/Home';
 import CourseHighlight from './components/CourseHighlight';
+import YouWillBuild from './components/YouWillBuild';
 
 const Courses = () => {
         return (
                 <div>
                         <div>
                                 <Home></Home>
+                        </div>
+                        <div>
+                                <YouWillBuild></YouWillBuild>
                         </div>
                         <div className='max-w-[1240px] mx-auto'>
                                 <CourseHighlight></CourseHighlight>
---
+++ src/pages/courses/components/YouWillBuild.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { FaLongArrowAltRight } from "react-icons/fa";
+
+
+const YouWillBuild = () => {
+        return (
+                <div className='project-section-bg mt-2'>
+                        <div className='max-w-[1240px] mx-auto'>
+                                <div>
+                                        <div className='pt-8 px-3 text-pink'>
+                                                <h1 className='text-xl md:text-3xl font-bold'>MERN stack web development</h1>
+                                                <h1 className='mt-3 text-xl md:text-3xl font-bold'>Projects that you will build</h1>
+                                        </div>
+                                        <div className='mt-2 grid md:grid-cols-3 gap-y-2 md:gap-y-8'>
+                                                <div className="card md:w-96 mx-4 md:px-0 card-shadow bg-[#fdf9fd]">
+                                                        <figure className="px-6 pt-6">
+                                                                <img src="https://files.codingninjas.in/phone-26004.svg" alt="Shoes" className="rounded-xl w-[100px] h-[100px]" />
+                                                        </figure>
+                                                        <div className='px-3'>
+                                                                <h2 className="card-title">Shoes!</h2>
+                                                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                                                <div>
+                                                                        <button className="allBtn bg-transparent text-black">Learn more<FaLongArrowAltRight></FaLongArrowAltRight> </button>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                                <div className="card md:w-96 mx-4 md:px-0 card-shadow bg-[#fdf9fd]">
+                                                        <figure className="px-6 pt-6">
+                                                                <img src="https://files.codingninjas.in/phone-26004.svg" alt="Shoes" className="rounded-xl w-[100px] h-[100px]" />
+                                                        </figure>
+                                                        <div className='px-3'>
+                                                                <h2 className="card-title">Shoes!</h2>
+                                                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                                                <div>
+                                                                        <button className="allBtn bg-transparent text-black">Learn more<FaLongArrowAltRight></FaLongArrowAltRight> </button>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                                <div className="card md:w-96 mx-4 md:px-0 card-shadow bg-[#fdf9fd]">
+                                                        <figure className="px-6 pt-6">
+                                                                <img src="https://files.codingninjas.in/phone-26004.svg" alt="Shoes" className="rounded-xl w-[100px] h-[100px]" />
+                                                        </figure>
+                                                        <div className='px-3'>
+                                                                <h2 className="card-title">Shoes!</h2>
+                                                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                                                <div>
+                                                                        <button className="allBtn bg-transparent text-black">Learn more<FaLongArrowAltRight></FaLongArrowAltRight> </button>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                                <div className="card md:w-96 mx-4 md:px-0 card-shadow bg-[#fdf9fd]">
+                                                        <figure className="px-6 pt-6">
+                                                                <img src="https://files.codingninjas.in/phone-26004.svg" alt="Shoes" className="rounded-xl w-[100px] h-[100px]" />
+                                                        </figure>
+                                                        <div className='px-3'>
+                                                                <h2 className="card-title">Shoes!</h2>
+                                                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                                                <div>
+                                                                        <button className="allBtn bg-transparent text-black">Learn more<FaLongArrowAltRight></FaLongArrowAltRight> </button>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                                <div className="card md:w-96 mx-4 md:px-0 card-shadow bg-[#fdf9fd]">
+                                                        <figure className="px-6 pt-6">
+                                                                <img src="https://files.codingninjas.in/phone-26004.svg" alt="Shoes" className="rounded-xl w-[100px] h-[100px]" />
+                                                        </figure>
+                                                        <div className='px-3'>
+                                                                <h2 className="card-title">Shoes!</h2>
+                                                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                                                <div>
+                                                                        <button className="allBtn bg-transparent text-black">Learn more<FaLongArrowAltRight></FaLongArrowAltRight> </button>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                                <div className="card md:w-96 mx-4 md:px-0 card-shadow bg-[#fdf9fd]">
+                                                        <figure className="px-6 pt-6">
+                                                                <img src="https://files.codingninjas.in/phone-26004.svg" alt="Shoes" className="rounded-xl w-[100px] h-[100px]" />
+                                                        </figure>
+                                                        <div className='px-3'>
+                                                                <h2 className="card-title">Shoes!</h2>
+                                                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                                                <div>
+                                                                        <button className="allBtn bg-transparent text-black">Learn more<FaLongArrowAltRight></FaLongArrowAltRight> </button>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        );
+};
+
+export default YouWillBuild;
