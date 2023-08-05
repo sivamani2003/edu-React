86174c5c5c3236248daf8ec29c1a9e0dec86f949
EmranAli-15 <mohammademranali15@gmail.com>
2023-08-05 16:03:58 +0600

    changed card design

--- src/pages/othersCourse/components/NewsBlogs.jsx
+++ src/pages/othersCourse/components/NewsBlogs.jsx
@@ -29,21 +29,21 @@ const NewsBlogs = () => {
                 }}
                 loop={true}
                 modules={[FreeMode, Pagination]}
-                className="mySwiper card-shadow"
+                className="mySwiper"
             >
                 <SwiperSlide>
-                    <div className="card shadow-xl">
+                    <div className="card news-blogs-card-shadow">
                         <figure>
                             <img src="http://www.baoiam.com/images/blog1.jpg" />
                         </figure>
                         <div className='relative'>
                             <div className='w-full p-2 rounded-t-[20px] text-white font-bold bg-red-500 absolute bottom-0 flex items-center justify-center gap-x-4'>
                                 <FaFolder size={30}></FaFolder>
-                                <p>Product Development</p>
+                                <p>Web Development</p>
                             </div>
                         </div>
                         <div className="card-body">
-                            <div className='flex items-center justify-center gap-x-1'>
+                            {/* <div className='flex items-center justify-center gap-x-1'>
                                 <div className='flex items-center text-xl gap-x-1'>
                                     <FaCalendarAlt className='text-pink'></FaCalendarAlt>
                                     <p className='text-gray'>March,30 2023</p>
@@ -53,89 +53,85 @@ const NewsBlogs = () => {
                                     <FaComment className='text-pink'></FaComment>
                                     <p className='text-gray'>Comments (3)</p>
                                 </div>
-                            </div>
+                            </div> */}
                             <p className='text-xl font-bold text-[#47476e] text-left'>Product development is not a course to learn, its an arts.</p>
                             <div className='text-left text-[#47476e] font-semibold'>
-                                <p>2. Course Duration: 3 month</p>
-                                <p>3. projects: 10 certifications</p>
-                                <p>4. 1:1 mentorship</p>
-                                <p>5. 12000k</p>
+                                <div className='flex justify-between'>
+                                    <div className='flex flex-col'>
+                                        <p>Duration: 3 month</p>
+                                        <p>projects: 10</p>
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <p>1:1 mentorship</p>
+                                        <p>Price: 12000k</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className='text-right'>
-                                <button className='text-xl font-bold text-[#47476e] hover:text-[#ee4962]'><span className='underline'>Read</span> More</button>
+                            <div className='text-right flex justify-center'>
+                                <button className='text-xl allBtn font-semibold'>Explore More</button>
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="card shadow-xl">
+                    <div className="card news-blogs-card-shadow">
                         <figure>
                             <img src="http://www.baoiam.com/images/blog2.jpg" />
                         </figure>
                         <div className='relative'>
                             <div className='w-full p-2 rounded-t-[20px] text-white font-bold bg-red-500 absolute bottom-0 flex items-center justify-center gap-x-4'>
                                 <FaFolder size={30}></FaFolder>
-                                <p>Enterpreneurship</p>
+                                <p>Machine Learning</p>
                             </div>
                         </div>
                         <div className="card-body">
-                            <div className='flex items-center justify-center gap-x-1'>
-                                <div className='flex items-center text-xl gap-x-1'>
-                                    <FaCalendarAlt className='text-pink'></FaCalendarAlt>
-                                    <p className='text-gray'>March,30 2023</p>
-                                </div>
-                                <hr className='h-[2px] w-5 rotate-90 bg-[#ee4962]' />
-                                <div className='flex items-center text-xl gap-x-1'>
-                                    <FaComment className='text-pink'></FaComment>
-                                    <p className='text-gray'>Comments (3)</p>
-                                </div>
-                            </div>
                             <p className='text-xl font-bold text-[#47476e] text-left'>The ERA on Enterpreneurship: How You can be the Next Success Story</p>
                             <div className='text-left text-[#47476e] font-semibold'>
-                                <p>2. Course Duration: 3 month</p>
-                                <p>3. projects: 10 certifications</p>
-                                <p>4. 1:1 mentorship</p>
-                                <p>5. 12000k</p>
+                                <div className='flex justify-between'>
+                                    <div className='flex flex-col'>
+                                        <p>Duration: 3 month</p>
+                                        <p>projects: 10</p>
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <p>1:1 mentorship</p>
+                                        <p>Price: 12000k</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className='text-right'>
-                                <button className='text-xl font-bold text-[#47476e] hover:text-[#ee4962]'><span className='underline'>Read</span> More</button>
+                            <div className='text-right flex justify-center'>
+                                <button className='text-xl allBtn font-semibold'>Explore More</button>
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="card shadow-xl">
+                    <div className="card news-blogs-card-shadow">
                         <figure>
                             <img src="http://www.baoiam.com/images/blog3.jpg" />
                         </figure>
                         <div className='relative'>
                             <div className='w-full p-2 rounded-t-[20px] text-white font-bold bg-red-500 absolute bottom-0 flex items-center justify-center gap-x-4'>
                                 <FaFolder size={30}></FaFolder>
-                                <p>Internship</p>
+                                <p>Python</p>
                             </div>
                         </div>
                         <div className="card-body">
-                            <div className='flex items-center justify-center gap-x-1'>
-                                <div className='flex items-center text-xl gap-x-1'>
-                                    <FaCalendarAlt className='text-pink'></FaCalendarAlt>
-                                    <p className='text-gray'>March,30 2023</p>
-                                </div>
-                                <hr className='h-[2px] w-5 rotate-90 bg-[#ee4962]' />
-                                <div className='flex items-center text-xl gap-x-1'>
-                                    <FaComment className='text-pink'></FaComment>
-                                    <p className='text-gray'>Comments (3)</p>
-                                </div>
-                            </div>
                             <p className='text-xl font-bold text-[#47476e] text-left'>Having an INTERNSHIP EXPERIENCE before getting info professional
-                                life is like ICING ON THE CAKE.</p>
+                            </p>
                             <div className='text-left text-[#47476e] font-semibold'>
-                                <p>2. Course Duration: 3 month</p>
-                                <p>3. projects: 10 certifications</p>
-                                <p>4. 1:1 mentorship</p>
-                                <p>5. 12000k</p>
+                                <div className='flex justify-between'>
+                                    <div className='flex flex-col'>
+                                        <p>Duration: 3 month</p>
+                                        <p>projects: 10</p>
+                                    </div>
+                                    <div className='flex flex-col'>
+                                        <p>1:1 mentorship</p>
+                                        <p>Price: 12000k</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className='text-right'>
-                                <button className='text-xl font-bold text-[#47476e] hover:text-[#ee4962]'><span className='underline'>Read</span> More</button>
+                            <div className='text-right flex justify-center'>
+                                <button className='text-xl allBtn font-semibold'>Explore More</button>
                             </div>
                         </div>
                     </div>
